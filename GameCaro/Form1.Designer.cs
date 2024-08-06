9c331abfaae1d6829a6cccb72db0549fd8c992b8
LeKhang-cn22h <chube02082017@gmail.com>
2024-08-06 23:24:27 +0700

    chuc nang quay lai

--- GameCaro/Form1.Designer.cs
+++ GameCaro/Form1.Designer.cs
@@ -56,18 +56,20 @@
             // pnlChessBoard
             // 
             pnlChessBoard.BackColor = SystemColors.Control;
-            pnlChessBoard.Location = new Point(9, 219);
+            pnlChessBoard.Location = new Point(11, 274);
+            pnlChessBoard.Margin = new Padding(4, 4, 4, 4);
             pnlChessBoard.Name = "pnlChessBoard";
-            pnlChessBoard.Size = new Size(813, 521);
+            pnlChessBoard.Size = new Size(1016, 651);
             pnlChessBoard.TabIndex = 0;
             // 
             // main_picture
             // 
             main_picture.Anchor = AnchorStyles.Top | AnchorStyles.Right;
             main_picture.BackgroundImage = Properties.Resources.Caro_Chess_5_in_a_line;
-            main_picture.Location = new Point(3, 3);
+            main_picture.Location = new Point(4, 4);
+            main_picture.Margin = new Padding(4, 4, 4, 4);
             main_picture.Name = "main_picture";
-            main_picture.Size = new Size(1129, 156);
+            main_picture.Size = new Size(1411, 195);
             main_picture.TabIndex = 0;
             main_picture.TabStop = false;
             // 
@@ -80,9 +82,10 @@
             panel3.Controls.Add(txbIP);
             panel3.Controls.Add(prcbCoolDown);
             panel3.Controls.Add(txbPlayerName);
-            panel3.Location = new Point(828, 219);
+            panel3.Location = new Point(1035, 274);
+            panel3.Margin = new Padding(4, 4, 4, 4);
             panel3.Name = "panel3";
-            panel3.Size = new Size(310, 521);
+            panel3.Size = new Size(388, 651);
             panel3.TabIndex = 2;
             // 
             // label1
@@ -90,9 +93,10 @@
             label1.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
             label1.AutoSize = true;
             label1.Font = new Font("Algerian", 18F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            label1.Location = new Point(17, 463);
+            label1.Location = new Point(21, 579);
+            label1.Margin = new Padding(4, 0, 4, 0);
             label1.Name = "label1";
-            label1.Size = new Size(272, 34);
+            label1.Size = new Size(323, 40);
             label1.TabIndex = 5;
             label1.Text = "5 in a line to win";
             // 
@@ -101,9 +105,10 @@
             pctbSign.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
             pctbSign.BackColor = SystemColors.Control;
             pctbSign.BackgroundImageLayout = ImageLayout.Stretch;
-            pctbSign.Location = new Point(7, 174);
+            pctbSign.Location = new Point(9, 218);
+            pctbSign.Margin = new Padding(4, 4, 4, 4);
             pctbSign.Name = "pctbSign";
-            pctbSign.Size = new Size(300, 262);
+            pctbSign.Size = new Size(375, 328);
             pctbSign.SizeMode = PictureBoxSizeMode.StretchImage;
             pctbSign.TabIndex = 4;
             pctbSign.TabStop = false;
@@ -111,9 +116,10 @@
             // btnLAN
             // 
             btnLAN.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
-            btnLAN.Location = new Point(5, 139);
+            btnLAN.Location = new Point(6, 174);
+            btnLAN.Margin = new Padding(4, 4, 4, 4);
             btnLAN.Name = "btnLAN";
-            btnLAN.Size = new Size(304, 29);
+            btnLAN.Size = new Size(380, 36);
             btnLAN.TabIndex = 3;
             btnLAN.Text = "Connect";
             btnLAN.UseVisualStyleBackColor = true;
@@ -121,26 +127,29 @@
             // txbIP
             // 
             txbIP.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
-            txbIP.Location = new Point(7, 91);
+            txbIP.Location = new Point(9, 114);
+            txbIP.Margin = new Padding(4, 4, 4, 4);
             txbIP.Name = "txbIP";
-            txbIP.Size = new Size(302, 27);
+            txbIP.Size = new Size(376, 31);
             txbIP.TabIndex = 2;
             // 
             // prcbCoolDown
             // 
             prcbCoolDown.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
-            prcbCoolDown.Location = new Point(7, 45);
+            prcbCoolDown.Location = new Point(9, 56);
+            prcbCoolDown.Margin = new Padding(4, 4, 4, 4);
             prcbCoolDown.Name = "prcbCoolDown";
-            prcbCoolDown.Size = new Size(300, 29);
+            prcbCoolDown.Size = new Size(375, 36);
             prcbCoolDown.TabIndex = 1;
             // 
             // txbPlayerName
             // 
             txbPlayerName.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
-            txbPlayerName.Location = new Point(5, 1);
+            txbPlayerName.Location = new Point(6, 1);
+            txbPlayerName.Margin = new Padding(4, 4, 4, 4);
             txbPlayerName.Name = "txbPlayerName";
             txbPlayerName.ReadOnly = true;
-            txbPlayerName.Size = new Size(302, 27);
+            txbPlayerName.Size = new Size(376, 31);
             txbPlayerName.TabIndex = 0;
             // 
             // tmCoolDown
@@ -150,9 +159,10 @@
             // panel2
             // 
             panel2.Controls.Add(main_picture);
-            panel2.Location = new Point(9, 43);
+            panel2.Location = new Point(11, 54);
+            panel2.Margin = new Padding(4, 4, 4, 4);
             panel2.Name = "panel2";
-            panel2.Size = new Size(1126, 159);
+            panel2.Size = new Size(1408, 199);
             panel2.TabIndex = 1;
             // 
             // menuStrip1
@@ -161,7 +171,8 @@
             menuStrip1.Items.AddRange(new ToolStripItem[] { menuToolStripMenuItem });
             menuStrip1.Location = new Point(0, 0);
             menuStrip1.Name = "menuStrip1";
-            menuStrip1.Size = new Size(1150, 28);
+            menuStrip1.Padding = new Padding(8, 2, 0, 2);
+            menuStrip1.Size = new Size(1438, 33);
             menuStrip1.TabIndex = 3;
             menuStrip1.Text = "menuStrip1";
             // 
@@ -169,14 +180,15 @@
             // 
             menuToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { newGameToolStripMenuItem, undoToolStripMenuItem, quitToolStripMenuItem });
             menuToolStripMenuItem.Name = "menuToolStripMenuItem";
-            menuToolStripMenuItem.Size = new Size(60, 24);
+            menuToolStripMenuItem.Size = new Size(73, 29);
             menuToolStripMenuItem.Text = "Menu";
+            menuToolStripMenuItem.Click += menuToolStripMenuItem_Click;
             // 
             // newGameToolStripMenuItem
             // 
             newGameToolStripMenuItem.Name = "newGameToolStripMenuItem";
             newGameToolStripMenuItem.ShortcutKeys = Keys.Control | Keys.N;
-            newGameToolStripMenuItem.Size = new Size(224, 26);
+            newGameToolStripMenuItem.Size = new Size(270, 34);
             newGameToolStripMenuItem.Text = "New game";
             newGameToolStripMenuItem.Click += newGameToolStripMenuItem_Click;
             // 
@@ -184,7 +196,7 @@
             // 
             undoToolStripMenuItem.Name = "undoToolStripMenuItem";
             undoToolStripMenuItem.ShortcutKeys = Keys.Control | Keys.Z;
-            undoToolStripMenuItem.Size = new Size(224, 26);
+            undoToolStripMenuItem.Size = new Size(270, 34);
             undoToolStripMenuItem.Text = "Undo";
             undoToolStripMenuItem.Click += undoToolStripMenuItem_Click;
             // 
@@ -192,21 +204,22 @@
             // 
             quitToolStripMenuItem.Name = "quitToolStripMenuItem";
             quitToolStripMenuItem.ShortcutKeys = Keys.Control | Keys.Q;
-            quitToolStripMenuItem.Size = new Size(224, 26);
+            quitToolStripMenuItem.Size = new Size(270, 34);
             quitToolStripMenuItem.Text = "Quit";
             quitToolStripMenuItem.Click += quitToolStripMenuItem_Click;
             // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1150, 752);
+            ClientSize = new Size(1438, 940);
             Controls.Add(panel3);
             Controls.Add(panel2);
             Controls.Add(pnlChessBoard);
             Controls.Add(menuStrip1);
             Icon = (Icon)resources.GetObject("$this.Icon");
             MainMenuStrip = menuStrip1;
+            Margin = new Padding(4, 4, 4, 4);
             Name = "Form1";
             Text = "Game Caro";
             FormClosing += Form1_FormClosing;
---
+++ GameCaro/PlayInfo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameCaro
+{
+    public class PlayInfo
+    {
+        private Point point;
+        public Point Point
+        {
+            get { return point; }
+            set { point = value; }
+        }
+        private int currentplayer;
+        public int Currentplayer
+        { get { return currentplayer; } set { currentplayer = value; } }
+        public PlayInfo(Point point, int currentplayer)
+        {
+            this.point = point;
+            this.currentplayer = currentplayer;
+        }
+    }   
+}
